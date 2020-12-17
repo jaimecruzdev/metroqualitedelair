c072fd6f21974762c8f9c1bb0d0cba1f09730a09
jaimecruzdev <52797267+jaimecruzdev@users.noreply.github.com>
2020-12-17 19:04:49 +0100

    Add files via upload
    Premiers fichiers du projet avec la présentation préliminaire et le code réalisé ensemble le premier jour

---
+++ Code/Recuperation des donnees/workingProjet.R
@@ -0,0 +1,40 @@
+#Test de récuperation des données à travers de JSON
+
+#import
+library(jsonlite)
+
+#url exemple
+annee2Read<-2013
+urlRATP<-"https://dataratp2.opendatasoft.com/api/records/1.0/search/?dataset=qualite-de-lair-mesuree-dans-la-station-franklin-d-roosevelt&q=&rows=10000&sort=-dateheure&facet=dateheure&refine.dateheure=2018"
+dfAux<-fromJSON(urlRATP)
+
+#boucle pour récupérer les données
+urlBase="https://dataratp2.opendatasoft.com/api/records/1.0/search/?dataset=qualite-de-lair-mesuree-dans-la-station-franklin-d-roosevelt&q=&rows=10000&sort=-dateheure&facet=dateheure&refine.dateheure="
+donAnnee=list()
+
+#dfAux$records$fields
+
+#lire par an
+for (anInd in 2013:2020)
+{
+  print(anInd)
+  urlRATP=paste(urlBase,anInd,sep="")
+  donAnnee[[anInd]]<-fromJSON(urlRATP)$records$fields
+}
+
+#on s'assure 
+for (anInd in 2013:2020)
+{
+  #print(head(donAnnee[[anInd]]$records$fields),5)
+  print(head(donAnnee[[anInd]]),5)
+}
+
+#Quelles années ?
+list_choix_an=list()
+
+
+
+
+
+
+
